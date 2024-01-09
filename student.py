af2c1bfe4dab24429bc341e6496c5f8dc0a699af
Bartosz Gembalczyk <bart.polska@gmail.com>
2024-01-09 16:25:38 +0100

    Zaktualizowano klase Student do nowych bardziej wymagających studentów i poprawiono camelCase na snake_case

--- student.py
+++ student.py
@@ -1,22 +1,23 @@
 from segment import Segment
 
 class Student:
-    def __init__(self, id: int, applicationTime: int, prefDorm, prefTenantsNum):
+    def __init__(self, id, year, sex, faculty, major, city, lang, preference):
         self.id = id
-        self.applicationTime = applicationTime
-        self.prefDorm = prefDorm
-        self.prefTenantsNum = prefTenantsNum
-        self.dorm = None
+        self.year = year
+        self.sex = sex
+        self.faculty = faculty
+        self.major = major
+        self.city = city
+        self.lang = lang
+        self.preference = preference
 
     def __str__(self):
-        return f"ID: {self.id}\n" \
-               f"Application time: {self.applicationTime}\n" \
-               f"Chosen dorm: {self.prefDorm}\n" \
-               f"Lives in: {self.dorm}"
+        return f"USOSid: {self.id}\n" \
+               f"Birth year: {self.year}\n" \
+               f"Sex: {self.prefDorm}\n" \
+               f"Student is studying: {self.major} in {self.lang} at the faculty {self.faculty} in {self.city}\n" \
+               f"Student's accommodation preferences: {self.preference}"
 
-    def __lt__(self, other):
-        return self.applicationTime < other.applicationTime
-
-    def accommodate(self, segmentVar: Segment) -> None:
-        segmentVar.tenants.append(self)
-        self.dorm = segmentVar.room().dorm()
+    def accommodate(self, segment_var: Segment) -> None:
+        segment_var.tenants.append(self)
+        self.dorm = segment_var.room().dorm()
