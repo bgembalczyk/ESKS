260d50f4a6964c54f04b8c3c5aa0d49a928d87e3
Bartosz Gembalczyk <bart.polska@gmail.com>
2024-01-23 16:09:37 +0100

    Poprawiono czytelnosc kodu w pliku room.py

--- room.py
+++ room.py
@@ -1,6 +1,6 @@
 import exceptions.segment
+import exceptions.room as error
 from segment import Segment
-from exceptions.room import *
 
 class Room:
     def __init__(self, dorm, number, habitable, condition, bathroom, kitchen, ad):
@@ -8,23 +8,25 @@ class Room:
         conditions = ["normal", "renovated", "old"]
         bathrooms = ["full", "shower", "null"]
 
+        # Validate input parameters
         if type(dorm) is not Dormitory:
-            raise WrongDorm
+            raise error.WrongDorm
         if type(number) is not int:
-            raise NumberNotInt
+            raise error.NumberNotInt
         if number <= 0:
-            raise WrongNumber
+            raise error.WrongNumber
         if type(habitable) is not bool:
-            raise WrongHabitable
+            raise error.WrongHabitable
         if condition not in conditions:
-            raise WrongCondition
+            raise error.WrongCondition
         if bathroom not in bathrooms:
-            raise WrongBathroom
+            raise error.WrongBathroom
         if type(kitchen) is not bool:
-            raise WrongKitchen
+            raise error.WrongKitchen
         if type(ad) is not bool:
-            raise WrongAd
+            raise error.WrongAd
 
+        # Assign values to instance variables
         self._dorm = dorm
         self._number = number
         self._bathroom = bathroom
@@ -55,18 +57,21 @@ class Room:
         return self._ad
 
     def beds(self) -> int:
+        # Calculate the total number of beds in all segments of the room
         res = 0
         for segment_iter in self.segments:
             res += segment_iter.beds
         return res
 
     def tenants_num(self) -> int:
+        # Calculate the total number of tenants in all segments of the room
         res = 0
         for segment in self.segments:
             res += len(segment.tenants)
         return res
 
     def __str__(self):
+        # Generate a formatted string representation of the Room object
         res = f"\tNumer pokoju: {self.number}\n" \
                f"\tLiczba łóżek: {self.beds()}\n" \
                f"\tLiczba mieszkańców: {self.tenants_num()}\n" \
@@ -74,9 +79,11 @@ class Room:
         return res
 
     def __lt__(self, other):
+        # Compare Room objects based on their room numbers
         return self.number < other.number
 
     def get_segment(self, segment_symbol) -> Segment | None:
+        # Retrieve a segment based on its symbol
         if type(segment_symbol) is not str:
             raise exceptions.segment.SymbolNotStr
         if len(segment_symbol) != 1 or segment_symbol not in "QWERTYUIOPASDFGHJKLZXCVBNM":
@@ -85,9 +92,10 @@ class Room:
         for segment_iter in self.segments:
             if segment_iter.symbol == segment_symbol:
                 return segment_iter
-        raise SegmentNotFound
+        raise error.SegmentNotFound
 
     def input_segments(self, tab: list):
+        # Add segments to the room based on the input data
         for segment in tab:
             new_segment = Segment(self, segment["symbol"], segment["habitable"], segment["beds"])
             self.segments.append(new_segment)
