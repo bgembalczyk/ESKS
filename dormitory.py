22cda30b586ec79c8820c268a742215ba92434a8
Bartosz Gembalczyk <bart.polska@gmail.com>
2024-01-23 16:13:45 +0100

    Poprawiono czytelnosc kodu w pliku dormitory.py.py

--- dormitory.py
+++ dormitory.py
@@ -1,6 +1,6 @@
 import exceptions.room
+import exceptions.dormitory as error
 from room import Room
-from exceptions.dormitory import *
 
 class Dormitory:
     def __init__(self, name, habitable, location):
@@ -8,13 +8,15 @@ class Dormitory:
                  "Ustronie", "Żaczek"]
         locations = ["Ochota", "Kampus Centralny", "Mokotów", "Wola", "Kampus Południowy"]
 
+        # Validate input parameters
         if name not in dorms:
-            raise WrongName
+            raise error.WrongName
         if location not in locations:
-            raise WrongLocation
+            raise error.WrongLocation
         if type(habitable) is not bool:
-            raise WrongHabitable
+            raise error.WrongHabitable
 
+        # Assign values to instance variables
         self._name = name
         self._location = location
         self.habitable = habitable
@@ -29,6 +31,7 @@ class Dormitory:
         return self._location
 
     def __str__(self):
+        # Generate a formatted string representation of the Dormitory object
         resStr = f"Dom Studencki {self.name}\n" \
                  f"Liczba pokoi: {len(self.rooms)}\n" \
                  f"Liczba łóżek: {self.beds()}\n" \
@@ -36,18 +39,21 @@ class Dormitory:
         return resStr
 
     def beds(self) -> int:
+        # Calculate the total number of beds in all rooms of the dormitory
         res = 0
         for room in self.rooms:
             res += room.beds()
         return res
 
     def tenants_num(self) -> int:
+        # Calculate the total number of tenants in all rooms of the dormitory
         res = 0
         for room in self.rooms:
             res += room.tenants_num()
         return res
 
     def get_room(self, roomNum) -> Room:
+        # Retrieve a room based on its number
         if type(roomNum) is not int:
             raise exceptions.room.NumberNotInt
         if roomNum <= 0:
@@ -56,9 +62,10 @@ class Dormitory:
         for room_iter in self.rooms:
             if room_iter.number == roomNum:
                 return room_iter
-        raise RoomNotFound
+        raise error.RoomNotFound
 
     def input_rooms(self, tab: list) -> None:
+        # Add rooms to the dormitory based on the input data
         for room in tab:
             new_room = Room(self, room["number"], room["habitable"], room["condition"], room["bathroom"],
                             room["kitchen"], room["ad"])
@@ -66,6 +73,7 @@ class Dormitory:
             self.rooms.append(new_room)
 
     def segment_types(self, repeating=False):
+        # Generate a list of unique or not SegmentType objects based on the dormitory's rooms and segments
         result = []
         if not self.habitable or self.beds() == self.tenants_num():
             return result
