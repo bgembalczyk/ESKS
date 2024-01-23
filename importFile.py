9673f0b2826a4e28e685da06d38562a1565579c9
Bartosz Gembalczyk <bart.polska@gmail.com>
2024-01-23 17:48:44 +0100

    Poprawiono czytelnosc kodu w pliku importFile.py

--- importFile.py
+++ importFile.py
@@ -4,6 +4,7 @@ from segmentType import SegmentType
 import json
 
 def input_dorms(path):
+    # Reads dormitory information from a JSON file and returns a list of Dormitory objects.
     with open(path, "r", encoding='utf-8') as file:
         dorms = []
         dorms_tmp = json.load(file)
@@ -14,52 +15,42 @@ def input_dorms(path):
     return dorms
 
 def input_students(path):
+    # Reads student information from a text file and returns a list of Student objects.
     with open(path, "r") as file:
         students = []
         for line in file:
             stud_tmp = line[:-1].split(";")
             stud_id, stud_year, stud_sex, stud_faculty, stud_major, stud_city, stud_lang, pref_roommate, pref_dorm, pref_segment, pref_location, pref_tenants_num_room, pref_tenants_num_segment, pref_condition, pref_bathroom, pref_kitchen, pref_ad = stud_tmp
-            try:
-                pref_roommate = eval(pref_roommate)
-            except (NameError, SyntaxError):
-                pass
-            try:
-                pref_dorm = eval(pref_dorm)
-            except (NameError, SyntaxError):
-                pass
-            try:
-                pref_segment = eval(pref_segment)
-            except (NameError, SyntaxError):
-                pass
-            try:
-                pref_location = eval(pref_location)
-            except (NameError, SyntaxError):
-                pass
-            try:
-                pref_tenants_num_room = eval(pref_tenants_num_room)
-            except (NameError, SyntaxError):
-                pass
-            try:
-                pref_tenants_num_segment = eval(pref_tenants_num_segment)
-            except (NameError, SyntaxError):
-                pass
-            try:
-                pref_condition = eval(pref_condition)
-            except (NameError, SyntaxError):
-                pass
-            try:
-                pref_bathroom = eval(pref_bathroom)
-            except (NameError, SyntaxError):
-                pass
-            try:
-                pref_kitchen = eval(pref_kitchen)
-            except (NameError, SyntaxError):
-                pass
-            try:
-                pref_ad = eval(pref_ad)
-            except (NameError, SyntaxError):
-                pass
-            stud_pref = SegmentType(pref_dorm, pref_location, pref_tenants_num_room, pref_tenants_num_segment, pref_condition, pref_bathroom, pref_kitchen, pref_ad)
-            new_stud = Student(int(stud_id), int(stud_year), stud_sex, stud_faculty, stud_major, stud_city, stud_lang, pref_roommate, (pref_dorm, pref_segment), stud_pref)
+
+            def eval_pref(pref):
+                # Helper function to evaluate preferences or set to None if evaluation fails
+                try:
+                    return eval(pref)
+                except (NameError, SyntaxError):
+                    return None
+
+            stud_pref = SegmentType(
+                eval_pref(pref_dorm),
+                eval_pref(pref_location),
+                eval_pref(pref_tenants_num_room),
+                eval_pref(pref_tenants_num_segment),
+                eval_pref(pref_condition),
+                eval_pref(pref_bathroom),
+                eval_pref(pref_kitchen),
+                eval_pref(pref_ad)
+            )
+
+            new_stud = Student(
+                int(stud_id),
+                int(stud_year),
+                stud_sex,
+                stud_faculty,
+                stud_major,
+                stud_city,
+                stud_lang,
+                eval_pref(pref_roommate),
+                (eval_pref(pref_dorm), eval_pref(pref_segment)),
+                stud_pref
+            )
             students.append(new_stud)
         return students
